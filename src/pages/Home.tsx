d53556d085ec687e16879add6448a3e79df5c312
Devon Stern <devon.stern@cleantelligent.com>
2022-04-06 12:54:19 -0600

    Reproducing bug

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,25 +1,54 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonAccordion, IonAccordionGroup, IonContent, IonHeader, IonIcon, IonItem, IonItemOption, IonItemOptions, IonItemSliding, IonLabel, IonList, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
+import { trash } from "ionicons/icons"
 import './Home.css';
 
 const Home: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+        <TestComp />
       </IonContent>
     </IonPage>
   );
 };
 
 export default Home;
+
+const TestComp: React.FC = () => {
+  const replicate = () => {
+      const items = []
+      for (let x = 0; x < 25; x++) {
+          items.push(item(x + ''))
+      }
+      return items
+  }
+
+  const item = (key: string): JSX.Element =>
+      <IonItemSliding key={key}>
+          <IonItem>
+              Item
+          </IonItem>
+          <IonItemOptions>
+              <IonItemOption onClick={() => console.log('text button')}>
+                  Click me
+              </IonItemOption>
+              <IonItemOption onClick={() => console.log('icon button')}>
+                  <IonIcon slot="icon-only" icon={trash} />
+              </IonItemOption>
+          </IonItemOptions>
+      </IonItemSliding>
+
+  return (
+      <IonAccordionGroup>
+          <IonAccordion>
+              <IonItem slot="header">
+                  <IonLabel>Label</IonLabel>
+              </IonItem>
+              <IonList slot="content">
+                  {replicate()}
+              </IonList>
+          </IonAccordion>
+      </IonAccordionGroup>
+  )
+}
